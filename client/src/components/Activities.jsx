4ce5dcf32cd70ff6a3b278fb21d463347e6c388a
Gabriel Bustos <gabrielbustos0905@gmail.com>
2023-07-22 18:06:01 -0300

    Activity

--- client/src/components/Activities.jsx
+++ client/src/components/Activities.jsx
@@ -3,6 +3,10 @@ import Ski from "../assets/ski.png";
 import Trekking from "../assets/trekking.jpg";
 import Tennis from "../assets/tennis.jpg";
 import CardActivity from "./CardActivity";
+import Autumn from "../assets/leaf-fall.png";
+import Spring from "../assets/flowers.png";
+import Summer from "../assets/sun.png";
+import Winter from "../assets/snowflake.png";
 
 const Activities = () => {
 
@@ -11,21 +15,21 @@ const Activities = () => {
             id: 1,
             name: "Ski",
             difficulty: 4,
-            season: ["Winter"],
+            season: [Winter],
             image: Ski
         },
         {
             id: 2,
             name: "Trekking",
             difficulty: 2,
-            season: ["Winter", "Summer", "Autum"],
+            season: [Winter, Summer, Autumn],
             image: Trekking
         },
         {
             id: 3,
             name: "Tennis",
             difficulty: 4,
-            season: ["Winter", "Summer", "Autum", "Spring"],
+            season: [Winter, Summer, Autumn, Spring],
             image: Tennis
         }
     ]
@@ -36,7 +40,7 @@ const Activities = () => {
                 <p className="text-4xl font-semibold text-gray-600 inline border-b-4 border-[#ad9efc]">Some Activities</p>
             </div>
 
-            <div className="h-full grid grid-cols-3 bg-black">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 pb-10">
                 {
                     activities?.map(({ id, name, difficulty, season, image}) => {
                         return (
--- client/src/components/CardActivity.jsx
+++ client/src/components/CardActivity.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
 const CardActivity = ({ id, name, difficulty, season, image }) => {
+    
     return (
-        <div className="h-auto w-60 flex flex-col bg-white">
-            <div className="flex justify-center items-center">
-                <img src={image} alt=""  className="h-40 w-56 mt-2"/>
-            </div>
+        <div className="flex justify-center items-center">
+            <div className="h-auto w-60 flex flex-col bg-white duration-200 hover:scale-105">
+                <div className="flex justify-center items-center">
+                    <img src={image} alt=""  className="h-40 w-56 mt-2"/>
+                </div>
 
-            <div>
-                <p>{name}</p>
+                <div className="flex justify-center items-center pt-4 pb-2">
+                    <p className="text-xl font-bold text-gray-600">{name}</p>
+                </div>
+
+                <div className="flex items-center pl-2">
+                    <p className="text-lg font-semibold text-gray-500 pr-2">Difficulty:</p>
+                    <p>{difficulty}</p>
+                </div>
+
+                <div className="flex items-center pl-2 pb-4 pt-2">
+                    <p className="text-lg font-semibold text-gray-500 pr-2">Season:</p>
+                    {
+                        season.map(s => {
+                            return (
+                                <img src={s} alt="" className="w-6 h-6 m-2"/>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
