c3f91143144b76b65d977fc2bedd6fbb3324a635
Gabriel Bustos <gabrielbustos0905@gmail.com>
2023-10-01 03:32:21 -0300

    No se que hice

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+      external: [
+        "leaflet/dist/Leaflet.css'"
+      ]
+    },
+  },
 })
